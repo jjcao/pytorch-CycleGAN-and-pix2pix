5e3f0299069095c1d91e1b49286c1f7577174972
JJCAO <jjcao1231@gmail.com>
2017-12-05 09:58:55 +0800

    bug for draw lines

--- util/util.py
+++ util/util.py
@@ -43,17 +43,17 @@ def naive_line(r0, c0, r1, c1):
 # jjcao
 def draw_2lines(im_arr, xy, im_size):
     x, y, val  = naive_line(*xy[0:4])
-    x[x>im_size-1]=im_size
+    x[x>im_size-1]=im_size-1
     x[x<0]=0
-    y[y>im_size-1]=im_size
+    y[y>im_size-1]=im_size-1
     y[y<0]=0
     
     im_arr[y, x, 0] = val * 255
     x, y, val  = naive_line(*xy[4:8])
-    x[x>im_size-1]=im_size
+    x[x>im_size-1]=im_size-1
     x[x<0]=0
-    y[y>im_size-1]=im_size
-    [y<0]=0
+    y[y>im_size-1]=im_size-1
+    y[y<0]=0
     im_arr[y, x, 0] = val * 255
     
 def diagnose_network(net, name='network'):
